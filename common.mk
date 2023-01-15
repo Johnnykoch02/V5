b4de8798a3a405c4a6f016ae65527ba0637a7ec0
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-01-15 01:39:59 -0500

    Configuration Creation

--- common.mk
+++ common.mk
@@ -17,7 +17,7 @@ MAKEDEPFOLDER = -$(VV)mkdir -p $(DEPDIR)/$$(dir $$(patsubst $(BINDIR)/%, %, $(RO
 RENAMEDEPENDENCYFILE = -$(VV)mv -f $(DEPDIR)/$$*.Td $$(patsubst $(SRCDIR)/%, $(DEPDIR)/%.d, $(ROOT)/$$<) && touch $$@
 
 LIBRARIES+=$(wildcard $(FWDIR)/*.a)
-LIBRARIES+= -ljsoncpp
+LIBRARIES+=ljsoncpp
 # Cannot include newlib and libc because not all of the req'd stubs are implemented
 EXCLUDE_COLD_LIBRARIES+=$(FWDIR)/libc.a $(FWDIR)/libm.a
 COLD_LIBRARIES=$(filter-out $(EXCLUDE_COLD_LIBRARIES), $(LIBRARIES))
