0d0f4e4809d59085554396de31eb388f18930e88
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-02-02 18:16:19 -0500

    Update Project and Library Development

--- common.mk
+++ common.mk
@@ -27,7 +27,7 @@ LNK_FLAGS=--gc-sections --start-group $(strip $(LIBRARIES)) -lgcc -lstdc++ --end
 ASMFLAGS=$(MFLAGS) $(WARNFLAGS)
 CFLAGS= $(MFLAGS) $(CPPFLAGS) $(WARNFLAGS) $(GCCFLAGS) --std=gnu17
 CXXFLAGS= $(MFLAGS) $(CPPFLAGS) $(WARNFLAGS) $(GCCFLAGS) --std=gnu++17
-LDFLAGS= $(MFLAGS) $(WARNFLAGS) -nostdlib $(GCCFLAGS)
+LDFLAGS= $(MFLAGS) $(WARNFLAGS) $(GCCFLAGS)
 SIZEFLAGS=-d --common
 NUMFMTFLAGS=--to=iec --format %.2f --suffix=B
 
