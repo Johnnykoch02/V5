fd63c329760ae1bab1724b98f9c411ff204414bc
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-03-06 01:14:43 -0500

    Debug

--- src/TerriBull/ProsAPI.cpp
+++ src/TerriBull/ProsAPI.cpp
@@ -17,6 +17,8 @@ pros::Motor m1(1, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor m2(2, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor m3(3, pros::E_MOTOR_GEARSET_18, true);
 pros::Motor m4(4, pros::E_MOTOR_GEARSET_18, true);
+
+pros::Imu mu(11);
 /*
  *
 */
@@ -27,3 +29,10 @@ void x_drive_motors_set_voltages(int v1, int v2, int v3, int v4) {
     m3.move(v3);
     m4.move(v4);
 }
+
+pros::Motor m5(1, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor m6(3, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor m7(2, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor m8(4, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor m9(6, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor m10(5, pros::E_MOTOR_GEARSET_18, false);
