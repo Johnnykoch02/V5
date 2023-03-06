9987390ddc2b8a959c810ee0994a2ebeb39429de
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-03-06 01:15:09 -0500

    Debug

--- src/TerriBull/ProsAPI.cpp
+++ src/TerriBull/ProsAPI.cpp
@@ -13,22 +13,22 @@
 /* 
  * X_Drive Configuration for BULL2
  */
-pros::Motor m1(1, pros::E_MOTOR_GEARSET_18, false);
-pros::Motor m2(2, pros::E_MOTOR_GEARSET_18, false);
-pros::Motor m3(3, pros::E_MOTOR_GEARSET_18, true);
-pros::Motor m4(4, pros::E_MOTOR_GEARSET_18, true);
+// pros::Motor m1(1, pros::E_MOTOR_GEARSET_18, false);
+// pros::Motor m2(2, pros::E_MOTOR_GEARSET_18, false);
+// pros::Motor m3(3, pros::E_MOTOR_GEARSET_18, true);
+// pros::Motor m4(4, pros::E_MOTOR_GEARSET_18, true);
 
 pros::Imu mu(11);
 /*
  *
 */
 
-void x_drive_motors_set_voltages(int v1, int v2, int v3, int v4) {
-    m1.move(v1);
-    m2.move(v2);
-    m3.move(v3);
-    m4.move(v4);
-}
+// void x_drive_motors_set_voltages(int v1, int v2, int v3, int v4) {
+//     m1.move(v1);
+//     m2.move(v2);
+//     m3.move(v3);
+//     m4.move(v4);
+// }
 
 pros::Motor m5(1, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor m6(3, pros::E_MOTOR_GEARSET_18, true);
