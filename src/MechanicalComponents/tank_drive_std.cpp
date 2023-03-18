f7f108cd173646015799b4ed240d5cff8dcb930b
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-03-18 02:36:56 -0400

    debug drive

--- src/MechanicalComponents/tank_drive_std.cpp
+++ src/MechanicalComponents/tank_drive_std.cpp
@@ -69,21 +69,13 @@ int Tank_Drive_Std::drive(TerriBull::Vector2 pos, float delta) {
     float offTrack = GetDTheta(RAD2DEG(dP->theta),  fmod(*(this->pCurrentAngle) + angleMod, 360));
     if (fabs(dP->r) > 4) {
         int dir = fabs(offTrack)/offTrack;
-        pL *= 0.9;
-        pR *= 0.9;
+        pL *= 0.92;
+        pR *= 0.92;
         pL += MIN(fabs(this->kPThetaTranslation*offTrack), fabs(0.1* pct)) * dir;
         pR -= MIN(fabs(this->kPThetaTranslation*offTrack), fabs(0.1* pct)) * dir;
     }
-    
-    else if(fabs(dP->r) > 2) { 
-        int dir = fabs(offTrack)/offTrack;
-        pL *= 0.5;
-        pR *= 0.5;
-        pL += MIN(fabs(this->kPThetaTranslation*offTrack), fabs(0.25* pct)) * dir;
-        pR -= MIN(fabs(this->kPThetaTranslation*offTrack), fabs(0.25* pct)) * dir;
-    }
 
-    else if (fabs(dP->r) > 0.5 && offTrack > 25) {
+    else if (fabs(dP->r) > 0.5 && offTrack > 15) {
         this->maneuverAngle(fmod(RAD2DEG(dP->theta) + angleMod, 360), delta);
         delete[] vals;
         delete dP;
@@ -128,7 +120,7 @@ void Tank_Drive_Std::maneuverAngle(float theta, float delta) {
   float* vals = new float[6];
   this->currentError = GetDTheta(theta, *(this->pCurrentAngle));
   this->sumError += this->currentError;
-  float pwr = this->kPTheta * this->currentError + this->kI * this->sumError + this->kDTheta * this->dError() / delta;
+  float pwr = this->kPTheta * this->currentError + this->kITheta * this->sumError + this->kDTheta * this->dError() / delta;
   std::stringstream s3;
   s3 << std::fixed << ::std::setprecision(1);
   s3 << "Err: "<< this->currentError << " Pwr: " << pwr;
